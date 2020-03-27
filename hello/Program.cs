3055f5a9db46d622f603dbb5701fc0748d545ce5
kushal <kushaldeveloper@gmail.com>
2020-03-27 13:40:41 -0600

    init project

---
+++ hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kushal!");
+            for (int i=0; i<10; i++)
+            {
+                Console.WriteLine("this is the " + i + " step.");
+            }
+        }
+    }
+}
