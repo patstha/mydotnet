91c89aa76932f361b5f14db4854a82c0db2b3eb2
kushal <kushaldeveloper@gmail.com>
2020-03-27 18:06:42 -0600

    add bubble sort

---
+++ hellolib/BubbleSort.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace hellolib
+{
+    public static class BubbleSort
+    {
+        public static int[] SortIntegers(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        int temp = input[j + 1];
+                        input[j + 1] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+    }
+}
