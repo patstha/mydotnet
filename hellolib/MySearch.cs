dd1a0a54630883a9009e2adb10ea4cbef98a3f31
kushal <kushaldeveloper@gmail.com>
2020-04-11 20:38:30 -0600

    fix binary search

--- hellolib/MySearch.cs
+++ hellolib/MySearch.cs
@@ -9,7 +9,7 @@ namespace hellolib
             for (int i = 0; i < arrayToSearch.Length; i++)
             {
                 if (arrayToSearch[i] == queryItem)
-                { 
+                {
                     return true;
                 }
             }
@@ -20,37 +20,24 @@ namespace hellolib
         {
             int[] inputArray = arrayToSearch;
             Array.Sort(inputArray);
-            int length = inputArray.Length;
             int lowerBound = 0;
-            int upperBound = length;
-            int midpoint = inputArray.Length / 2;
-            int midpointValue = inputArray[midpoint];
-            if (CheckIfQueryIsTheCentralTerm(inputArray, lowerBound, upperBound, queryItem)) 
+            int upperBound = inputArray.Length - 1;
+            int midPoint;
+            while (lowerBound <= upperBound)
             {
-                return true;
-            }
-            else if (midpointValue < queryItem)
-            {
-                // search the lower half
-                lowerBound = 0;
-                upperBound = midpoint - 1;
-            }
-            else if (midpointValue > queryItem)
-            {
-                // search the upper half 
-                lowerBound = midpoint + 1; 
-                upperBound = length;
-            }
-            return false;
-        }
-
-        private static bool CheckIfQueryIsTheCentralTerm(int[] inputArray, int lowerBound, int upperBound, int queryItem)
-        {
-            int midpoint = (upperBound - lowerBound) / 2;
-            int midpointValue = inputArray[midpoint];
-            if (midpointValue == queryItem) 
-            {
-                return true;
+                midPoint = (upperBound + lowerBound) / 2;
+                if (inputArray[midPoint] == queryItem)
+                {
+                    return true;
+                } 
+                else if (inputArray[midPoint] < queryItem)
+                {
+                    lowerBound = midPoint + 1;
+                } 
+                else if (inputArray[midPoint] > queryItem)
+                {
+                    upperBound = midPoint - 1;
+                }
             }
             return false;
         }
--- tests/MySearchTests.cs
+++ tests/MySearchTests.cs
@@ -33,5 +33,19 @@ namespace tests
             int queryItem = 9;
             Assert.False(MySearch.BinarySearchIntegers(arrayToSearch, queryItem));
         }
+                [Fact]
+        public void BinarySearchDoesExist()
+        {
+            int[] arrayToSearch = { 1, 2, 3, 4, 5, 6 };
+            int queryItem = 1;
+            Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem));
+        }
+        [Fact]
+        public void BinarySearchDoesExistOdd()
+        {
+            int[] arrayToSearch = { 1, 2, 3, 4, 5, 6, 7 };
+            int queryItem = 1;
+            Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem));
+        }
     }
 }
