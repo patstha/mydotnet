add509567a7e85f20c87d2ba6bd9d44f736b4db3
kushal <kushaldeveloper@gmail.com>
2020-07-15 14:59:29 -0600

    wip all passed

--- hello/Program.cs
+++ hello/Program.cs
@@ -31,28 +31,40 @@ class Result
 
     public static List<string> fetchItemsToDisplay(List<List<string>> items, int sortParameter, int sortOrder, int itemsPerPage, int pageNumber)
     {
-        foreach (var x in items)
-        {
-            foreach (var y in x)
-            {
-                Console.Write(y);
-                Console.Write(" ");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine($"{items.Count}");
-        Console.WriteLine($"{sortParameter}");
-        Console.WriteLine($"{sortOrder}");
-        Console.WriteLine($"{itemsPerPage}");
-        Console.WriteLine($"{pageNumber}");
-        IEnumerable < List<string> > orderedItems;
+        // foreach (var x in items)
+        // {
+        //     foreach (var y in x)
+        //     {
+        //         Console.Write(y);
+        //         Console.Write(" ");
+        //     }
+        //     Console.WriteLine();
+        // }
+        // Console.WriteLine($"{items.Count}");
+        // Console.WriteLine($"{sortParameter}");
+        // Console.WriteLine($"{sortOrder}");
+        // Console.WriteLine($"{itemsPerPage}");
+        // Console.WriteLine($"{pageNumber}");
+        IEnumerable<List<string>> orderedItems;
         if (sortOrder == 0)
         {
-            orderedItems = items.OrderBy(x => x[sortParameter]);
+            if (sortParameter > 0)
+            {
+                orderedItems = items.OrderBy(x => Int32.Parse(x[sortParameter]));
+            } else 
+            {
+                orderedItems = items.OrderBy(x => x[sortParameter]);
+            }
         }
         else
         {
-            orderedItems = items.OrderByDescending(x => x[sortParameter]);
+            if (sortParameter > 0)
+            {
+                orderedItems = items.OrderByDescending(x => Int32.Parse(x[sortParameter]));
+            } else 
+            {
+                orderedItems = items.OrderByDescending(x => x[sortParameter]);
+            }
         }
         IEnumerable<List<string>> filteredItems = orderedItems.Skip(pageNumber * itemsPerPage).Take(itemsPerPage);
         List<string> result = new List<string>();
@@ -81,29 +93,29 @@ class Solution
         //     items.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
         // }
 
-        // items.Add("p1 1 2".Split(' ').ToList());
-        // items.Add("p2 2 1".Split(' ').ToList());
+        items.Add("p1 1 2".Split(' ').ToList());
+        items.Add("p2 2 1".Split(' ').ToList());
 
-        // int sortParameter = 0;
+        int sortParameter = 0;
+
+        int sortOrder = 0;
+
+        int itemsPerPage = 1;
+
+        int pageNumber = 0;
+
+        // items.Add("owjevtkuyv 58584272 62930912".Split(' ').ToList());
+        // items.Add("rpaqgbjxik 9425650 96088250".Split(' ').ToList());
+        // items.Add("dfbkasyqcn 37469674 46363902".Split(' ').ToList());
+        // items.Add("vjrrisdfxe 18666489 88046739".Split(' ').ToList());
+
+        // int sortParameter = 1;
 
         // int sortOrder = 0;
 
         // int itemsPerPage = 1;
 
-        // int pageNumber = 0;
-
-        items.Add("owjevtkuyv 58584272 62930912".Split(' ').ToList());
-        items.Add("rpaqgbjxik 9425650 96088250".Split(' ').ToList());
-        items.Add("dfbkasyqcn 37469674 46363902".Split(' ').ToList());
-        items.Add("vjrrisdfxe 18666489 88046739".Split(' ').ToList());
-
-        int sortParameter = 2;
-
-        int sortOrder = 1;
-
-        int itemsPerPage = 2;
-
-        int pageNumber = 1;
+        // int pageNumber = 1;
 
         List<string> result = Result.fetchItemsToDisplay(items, sortParameter, sortOrder, itemsPerPage, pageNumber);
 
