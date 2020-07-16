4def400adda986f937deafbb0bdf28a42df0f8b5
kushal <kushaldeveloper@gmail.com>
2020-07-15 18:28:21 -0600

    add a test by @freqlabs

--- tests/MinimumCoinTests.cs
+++ tests/MinimumCoinTests.cs
@@ -8,10 +8,10 @@ namespace tests
     public class MinimumCoinTests
     {
         MinimumCoin minimumCoin;
-        public MinimumCoinTests() 
+        public MinimumCoinTests()
         {
-            var coinValues = new List<int>(new int[]{ 1, 5, 10, 25 });
-            this.minimumCoin = new MinimumCoin(CoinSet: coinValues, RepeatFactor: 6);
+            var coinValues = new List<int>() { 1, 5, 10, 25 };
+            minimumCoin = new MinimumCoin(CoinSet: coinValues, RepeatFactor: 6);
         }
 
         [Fact]
@@ -23,25 +23,25 @@ namespace tests
         [Fact]
         public void GetTrivialCaseOfZeroCoin()
         {
-            Assert.Equal(0, this.minimumCoin.getCount(0));
+            Assert.Equal(0, minimumCoin.getCount(0));
         }
 
         [Fact]
         public void GetTrivialCaseOfOneCoin()
         {
-            foreach(int i in this.minimumCoin.CoinSet)
+            foreach (int i in minimumCoin.CoinSet)
             {
-                Assert.Equal(1, this.minimumCoin.getCount(i));
+                Assert.Equal(1, minimumCoin.getCount(i));
             }
         }
 
         [Fact]
         public void GetCaseOfTwoCoins()
         {
-            if (this.minimumCoin.CoinSet.Count > 1 && !this.minimumCoin.CoinSet.Contains(this.minimumCoin.CoinSet[0] + this.minimumCoin.CoinSet[1]))
+            if (minimumCoin.CoinSet.Count > 1 && !minimumCoin.CoinSet.Contains(minimumCoin.CoinSet[0] + minimumCoin.CoinSet[1]))
             {
-                int total = this.minimumCoin.CoinSet[0] + this.minimumCoin.CoinSet[1];
-                int actual = this.minimumCoin.getCount(total);
+                int total = minimumCoin.CoinSet[0] + minimumCoin.CoinSet[1];
+                int actual = minimumCoin.getCount(total);
                 Assert.Equal(2, actual);
             }
         }
@@ -49,10 +49,11 @@ namespace tests
         [Fact]
         public void GetCaseOfFourCoins()
         {
-            int total = this.minimumCoin.CoinSet[0] + this.minimumCoin.CoinSet[1] + this.minimumCoin.CoinSet[2] + this.minimumCoin.CoinSet[3];
-            if (this.minimumCoin.CoinSet.Count > 1 && !this.minimumCoin.CoinSet.Contains(total))
-            {;
-                int actual = this.minimumCoin.getCount(total);
+            int total = minimumCoin.CoinSet[0] + minimumCoin.CoinSet[1] + minimumCoin.CoinSet[2] + minimumCoin.CoinSet[3];
+            if (minimumCoin.CoinSet.Count > 1 && !minimumCoin.CoinSet.Contains(total))
+            {
+                ;
+                int actual = minimumCoin.getCount(total);
                 Assert.Equal(4, actual);
             }
         }
@@ -60,12 +61,21 @@ namespace tests
         [Fact]
         public void GetCaseOfEightCoins()
         {
-            int total = this.minimumCoin.CoinSet[0] + this.minimumCoin.CoinSet[1] + this.minimumCoin.CoinSet[2] + this.minimumCoin.CoinSet[3] + this.minimumCoin.CoinSet[0] + this.minimumCoin.CoinSet[1] + this.minimumCoin.CoinSet[2] + this.minimumCoin.CoinSet[3];
-            if (this.minimumCoin.CoinSet.Count > 1 && !this.minimumCoin.CoinSet.Contains(total))
-            {;
-                int actual = this.minimumCoin.getCount(total);
+            int total = minimumCoin.CoinSet[0] + minimumCoin.CoinSet[1] + minimumCoin.CoinSet[2] + minimumCoin.CoinSet[3] + minimumCoin.CoinSet[0] + minimumCoin.CoinSet[1] + minimumCoin.CoinSet[2] + minimumCoin.CoinSet[3];
+            if (minimumCoin.CoinSet.Count > 1 && !minimumCoin.CoinSet.Contains(total))
+            {
+                ;
+                int actual = minimumCoin.getCount(total);
                 Assert.True(6 >= actual);
             }
+        }
+
+        [Fact]
+        public void GetFreqLabsTwoCoins()
+        {
+            var coinValues = new List<int>() { 1, 120, 200 };
+            minimumCoin = new MinimumCoin(CoinSet: coinValues, RepeatFactor: 6);
+            Assert.Equal(2, minimumCoin.getCount(240));
         }
     }
 }
