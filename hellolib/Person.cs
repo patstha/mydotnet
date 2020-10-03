a028c8894a5d7dce3869fef7bcff06f656373cb1
kushal <kushaldeveloper@gmail.com>
2020-10-02 21:35:52 -0600

    remove newlines

--- hellolib/Person.cs
+++ hellolib/Person.cs
@@ -5,15 +5,11 @@ namespace hellolib
     public class Person
     {
         public string Name { get; }
-
         public string Password { get; }
-
         internal Person(string name, string password)
         {
             Name = name;
             Password = password;
         }
-
-
     }
 }
