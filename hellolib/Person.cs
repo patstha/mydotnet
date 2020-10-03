b4b3efaeb163b8811ac05be7f6eb5cff89719473
kushal <kushaldeveloper@gmail.com>
2020-10-02 21:26:16 -0600

    use a factory
    fixes #4

--- hellolib/Person.cs
+++ hellolib/Person.cs
@@ -4,24 +4,16 @@ namespace hellolib
 {
     public class Person
     {
-        private static readonly int MINIMUM_PASSWORD_LENGTH = 8;
-        private static readonly int MAXIMUM_PASSWORD_LENGTH = 128;
-        public string Name { get; set; }
+        public string Name { get; }
 
-        public string Password { get; set; }
+        public string Password { get; }
 
-        public bool CheckPasswordMeetsRequirements(string password)
+        internal Person(string name, string password)
         {
-            int length = password.Length;
-            if (length < MINIMUM_PASSWORD_LENGTH)
-            {
-                return false;
-            }
-            if (length > MAXIMUM_PASSWORD_LENGTH)
-            {
-                return false;
-            }
-            return true;
+            Name = name;
+            Password = password;
         }
+
+
     }
 }
