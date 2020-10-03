ed2703bb2775b99402bafb4a6c9755406717ba48
Pat <pat@tildeteam.org>
2020-10-03 14:12:53 +0000

    enhance project
    add all things

--- hellolib/Entity.cs
+++ hellolib/Entity.cs
@@ -7,5 +7,7 @@ namespace hellolib
         public Guid Guid { get; set; } = Guid.NewGuid(); 
         public string CreatedBy { get; set; } = "System";
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+	public string ModifiedBy { get; set; } 
+	public DateTime ModifiedDate { get; set; }
     }
 }
