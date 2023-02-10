5dd89016a06acdce7b708a9b29dc406ac4544607
kushal <kushaldeveloper@gmail.com>
2023-02-10 06:58:32 -0600

    wip

--- SalesforceMapper/Mapper.cs
+++ SalesforceMapper/Mapper.cs
@@ -1,9 +1,18 @@
-﻿namespace SalesforceMapper;
+﻿using Newtonsoft.Json;
+
+namespace SalesforceMapper;
 
 public static class Mapper
 {
     public static int AddTwoIntegers(int first, int second)
     {
         return first + second;
+    }
+
+    public static string ManipulateJsonString(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json)) { return "";  }
+        dynamic? input = JsonConvert.DeserializeObject(json);
+        return "";
     }
 }
--- tests/MapperTests.cs
+++ tests/MapperTests.cs
@@ -27,5 +27,19 @@ namespace tests
             // assert
             actual.Should().Be(expected);
         }
+
+        [Theory]
+        [InlineData(
+            "{\"Id\": \"Abraham Lincoln\", \"ContactEmail__c\": \"abe@whitehouse.gov\"}", 
+            "{\"Preferences\": [ { \"PrefCode\": \"email__c\" } }"
+        )]
+        public void ManipulateJsonString_ShouldReturnExpectedJsonString(string json, string expected)
+        {
+            // arrange, act
+            string actual = Mapper.ManipulateJsonString(json);
+
+            // assert 
+            expected.Should().Be(actual);
+        }
     }
 }
