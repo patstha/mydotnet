a5a507921cea703f8b5cfec3f5b0e667dc1f2522
Kushal <kushaldeveloper@gmail.com>
2023-05-14 12:32:16 -0500

    add error
     tests.PersonTests.ShortOrLongPasswordsFail(name: "Pratikchhya Shrestha", password: "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwx"...)
       Source: PersonTests.cs line 39
       Duration: 49 ms
    
      Message: 
    Expected a <System.ArgumentNullException> to be thrown, but found <System.ArgumentException>: "
    "System.ArgumentException: The password provided to create user Pratikchhya Shrestha is not valid. A password must have a minimum length no shorter than 8 and no longer than 128.
       at hellolib.PersonFactory.Create(String name, String password) in C:\Users\kushal\src\mydotnet\hellolib\PersonFactory.cs:line 9
       at tests.PersonTests.<>c__DisplayClass3_0.<ShortOrLongPasswordsFail>b__0() in C:\Users\kushal\src\mydotnet\tests\PersonTests.cs:line 43
       at FluentAssertions.Specialized.FunctionAssertions`1.InvokeSubject()
       at FluentAssertions.Specialized.DelegateAssertions`2.InvokeSubjectWithInterception().
    
      Stack Trace: 
    XUnit2TestFramework.Throw(String message)
    TestFrameworkProvider.Throw(String message)
    DefaultAssertionStrategy.HandleFailure(String message)
    AssertionScope.FailWith(Func`1 failReasonFunc)
    AssertionScope.FailWith(Func`1 failReasonFunc)
    AssertionScope.FailWith(String message, Object[] args)
    DelegateAssertionsBase`2.ThrowInternal[TException](Exception exception, String because, Object[] becauseArgs)
    DelegateAssertions`2.Throw[TException](String because, Object[] becauseArgs)
    PersonTests.ShortOrLongPasswordsFail(String name, String password) line 44
    RuntimeMethodHandle.InvokeMethod(Object target, Void** arguments, Signature sig, Boolean isConstructor)
    MethodInvoker.Invoke(Object obj, IntPtr* args, BindingFlags invokeAttr)
    
     tests.PersonTests.ShortOrLongPasswordsFail(name: "Pratikchhya Shrestha", password: "hunter2")
       Source: PersonTests.cs line 39
       Duration: 1 ms
    
      Message: 
    Expected a <System.ArgumentNullException> to be thrown, but found <System.ArgumentException>: "
    "System.ArgumentException: The password provided to create user Pratikchhya Shrestha is not valid. A password must have a minimum length no shorter than 8 and no longer than 128.
       at hellolib.PersonFactory.Create(String name, String password) in C:\Users\kushal\src\mydotnet\hellolib\PersonFactory.cs:line 9
       at tests.PersonTests.<>c__DisplayClass3_0.<ShortOrLongPasswordsFail>b__0() in C:\Users\kushal\src\mydotnet\tests\PersonTests.cs:line 43
       at FluentAssertions.Specialized.FunctionAssertions`1.InvokeSubject()
       at FluentAssertions.Specialized.DelegateAssertions`2.InvokeSubjectWithInterception().
    
      Stack Trace: 
    XUnit2TestFramework.Throw(String message)
    TestFrameworkProvider.Throw(String message)
    DefaultAssertionStrategy.HandleFailure(String message)
    AssertionScope.FailWith(Func`1 failReasonFunc)
    AssertionScope.FailWith(Func`1 failReasonFunc)
    AssertionScope.FailWith(String message, Object[] args)
    DelegateAssertionsBase`2.ThrowInternal[TException](Exception exception, String because, Object[] becauseArgs)
    DelegateAssertions`2.Throw[TException](String because, Object[] becauseArgs)
    PersonTests.ShortOrLongPasswordsFail(String name, String password) line 44
    InvokeStub_PersonTests.ShortOrLongPasswordsFail(Object, Object, IntPtr*)
    MethodInvoker.Invoke(Object obj, IntPtr* args, BindingFlags invokeAttr)

--- tests/PersonTests.cs
+++ tests/PersonTests.cs
@@ -40,6 +40,7 @@ public class PersonTests
     {
         int MINIMUM_PASSWORD_LENGTH = 8;
         int MAXIMUM_PASSWORD_LENGTH = 128;
-        PersonFactory.Create(name, password).Should().Throw<ArgumentNullException>().WithMessage($"The password provided to create user {name} is not valid. A password must have a minimum length no shorter than {MINIMUM_PASSWORD_LENGTH} and no longer than {MAXIMUM_PASSWORD_LENGTH}.");
+        Func<Person> func = () => PersonFactory.Create(name, password);
+        func.Should().Throw<ArgumentNullException>().WithMessage($"The password provided to create user {name} is not valid. A password must have a minimum length no shorter than {MINIMUM_PASSWORD_LENGTH} and no longer than {MAXIMUM_PASSWORD_LENGTH}.");
     }
 }
