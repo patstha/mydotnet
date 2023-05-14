90f54601067aefc9414b4171053912df661f293a
Kushal <kushaldeveloper@gmail.com>
2023-05-14 12:35:17 -0500

    fix test

--- tests/PersonTests.cs
+++ tests/PersonTests.cs
@@ -38,9 +38,14 @@ public class PersonTests
     [InlineData("Pratikchhya Shrestha", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")]
     public void ShortOrLongPasswordsFail(string name, string password)
     {
+        // Arrange
         int MINIMUM_PASSWORD_LENGTH = 8;
         int MAXIMUM_PASSWORD_LENGTH = 128;
-        Func<Person> func = () => PersonFactory.Create(name, password);
-        func.Should().Throw<ArgumentNullException>().WithMessage($"The password provided to create user {name} is not valid. A password must have a minimum length no shorter than {MINIMUM_PASSWORD_LENGTH} and no longer than {MAXIMUM_PASSWORD_LENGTH}.");
+
+        // Act
+        Action act = () => PersonFactory.Create(name, password);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage($"The password provided to create user {name} is not valid. A password must have a minimum length no shorter than {MINIMUM_PASSWORD_LENGTH} and no longer than {MAXIMUM_PASSWORD_LENGTH}.");
     }
 }
