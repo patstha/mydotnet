f28a75f0770c16474d589bcb53b78fd03512c0ca
Kushal <kushaldeveloper@gmail.com>
2024-07-08 13:15:03 -0400

    add a failing test case

---
+++ hellolib/MyCleaner.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace hellolib;
+
+public class MyCleaner
+{
+    public static string CleanUrl(string url)
+    {
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            return "";
+        }
+        Uri uri = new(url);
+        string returnUrl = HttpUtility.ParseQueryString(uri.Query).Get("return");
+        if (returnUrl != null)
+        {
+            string decodedUrl = HttpUtility.UrlDecode(returnUrl);
+            string finalUrl = HttpUtility.ParseQueryString(new Uri(decodedUrl).Query).Get("u");
+            if (finalUrl != null)
+            {
+                return HttpUtility.UrlDecode(finalUrl);
+            }
+        }
+        return null;
+    }
+}
---
+++ tests/BackoffExceptionTests.cs
@@ -0,0 +1,14 @@
+﻿using hellolib;
+using System.Threading.Tasks;
+
+namespace tests;
+
+public class BackoffExceptionTests
+{
+    [Fact]
+    public async Task DoSomething()
+    {
+        BackoffException backoffException = new();
+        await backoffException.BackoffSelfinflictedException("name");
+    }
+}
