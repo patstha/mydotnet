fdd6059619d42f7821e4535ac4a76811289805fb
Kushal <kushaldeveloper@gmail.com>
2024-07-08 13:28:46 -0400

    add another failing test

--- tests/MyCleanerTests.cs
+++ tests/MyCleanerTests.cs
@@ -28,6 +28,7 @@ public class MyCleanerTests
     [Theory]
     [InlineData("https://www.ojrq.net/p/?return=https%3A%2F%2Felgato.sjv.io%2Fc%2F1378764%2F1064223%2F13666%3Fu%3Dhttps%253A%252F%252Fwww.elgato.com%252Fus%252Fen%252Fp%252Ffacecam-neo%26svlink%3D10538211%26level%3D1&cid=13666&tpsync=yes&auth=5781a01fa17f6b33", "https://www.elgato.com/us/en/p/facecam-neo")]
     [InlineData("https://www.tkqlhce.com/click-4485850-13112209?sid=e4e977b23d4c11ef8314763100633dd80INT&url=https%3A%2F%2Fcomputers.woot.com%2Foffers%2Flogitech-g920-driving-force-racing-wheel-and-floor-pedals-3", "https://computers.woot.com/offers/logitech-g920-driving-force-racing-wheel-and-floor-pedals-3")]
+    [InlineData("https://goto.walmart.com/c/2189989/612734/9383?subid1=def01ed63d4e11efb8829a5345c674ba0INT&sourceid=imp_000011112222333344&u=https%3A%2F%2Fwww.walmart.com%2Fplus", "https://www.walmart.com/plus")]
     public void CleanUrl_ShouldReturnValidOutput(string input, string expectedOutput)
     {
         // Act 
