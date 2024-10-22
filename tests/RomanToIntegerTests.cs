75b6a04db581bc2808a98195e3a9ccffb579df71
Kushal <kushaldeveloper@gmail.com>
2024-10-22 12:35:52 -0400

    add a failing test case

---
+++ tests/RomanToIntegerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace tests;
+
+public class RomanToIntegerTests
+{
+    [Fact]
+    public void RomanToInt_ShouldReturn1()
+    {
+        // arrange 
+        string s = "III";
+        int expected = 3;
+
+        // act 
+        int actual = RomanToInteger.RomanToInt(s);
+
+        // assert 
+        actual.Should().Be(expected);
+    }
+}
