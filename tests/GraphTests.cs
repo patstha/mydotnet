6f401b88c148b3e6f13e990f8477abdc1141bfc6
Kushal <kushaldeveloper@gmail.com>
2024-10-28 20:07:00 -0400

    add tests
    next I have graph for which not all branches are covered
    namespace hellolib;
    
    public class Graph
    {
        private readonly Dictionary<string, List<string>> _adjacencyList = new();
    
        public void AddNode(string node)
        {
            if (!_adjacencyList.ContainsKey(node))
            {
                _adjacencyList[node] = [];
            }
        }
    
        public void AddEdge(string from, string to)
        {
            if (_adjacencyList.ContainsKey(from) && _adjacencyList.ContainsKey(to))
            {
                _adjacencyList[from].Add(to);
            }
        }
    
        public List<string> Bfs(string start)
        {
            HashSet<string> visited = [];
            Queue<string> queue = new();
            List<string> result = [];
    
            queue.Enqueue(start);
            visited.Add(start);
    
            while (queue.Count > 0)
            {
                string node = queue.Dequeue();
                result.Add(node);
    
                foreach (string neighbor in _adjacencyList[node].Where(neighbor => !visited.Contains(neighbor)))
                {
                    queue.Enqueue(neighbor);
                    visited.Add(neighbor);
                }
            }
    
            return result;
        }
    
        public List<string> Dfs(string start)
        {
            HashSet<string> visited = [];
            List<string> result = [];
            DfsRecursive(start, visited, result);
            return result;
        }
    
        private void DfsRecursive(string node, HashSet<string> visited, List<string> result)
        {
            visited.Add(node);
            result.Add(node);
    
            foreach (string neighbor in _adjacencyList[node].Where(neighbor => !visited.Contains(neighbor)))
            {
                DfsRecursive(neighbor, visited, result);
            }
        }
    }
    using System.Collections.Generic;
    
    namespace tests;
    
    public class GraphTests
    {
        [Fact]
        public void BFS_ShouldFindElement()
        {
            Graph graph = new();
    
            // Add nodes
            graph.AddNode("A");
            graph.AddNode("B");
            graph.AddNode("C");
            graph.AddNode("D");
    
            // Add edges
            graph.AddEdge("A", "B");
            graph.AddEdge("A", "C");
            graph.AddEdge("B", "D");
            graph.AddEdge("C", "D");
    
            // Perform BFS and DFS
            List<string> bfsResult = graph.Bfs("A");
            bfsResult.Should().Contain("A");
        }
        [Fact]
        public void DFS_ShouldFindElement()
        {
            Graph graph = new();
    
            // Add nodes
            graph.AddNode("A");
            graph.AddNode("B");
            graph.AddNode("C");
            graph.AddNode("D");
    
            // Add edges
            graph.AddEdge("A", "B");
            graph.AddEdge("A", "C");
            graph.AddEdge("B", "D");
            graph.AddEdge("C", "D");
    
            // Perform BFS and DFS
            List<string> dfsResult = graph.Dfs("A");
            dfsResult.Should().Contain("A");
        }
    }

--- tests/GraphTests.cs
+++ tests/GraphTests.cs
@@ -1,49 +1,208 @@
 ﻿using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
 
-namespace tests;
-
-public class GraphTests
+namespace tests
 {
-    [Fact]
-    public void BFS_ShouldFindElement()
+    public class GraphTests
     {
-        Graph graph = new();
+        [Fact]
+        public void Freebie()
+        {
+            Assert.True(true);
+        }
 
-        // Add nodes
-        graph.AddNode("A");
-        graph.AddNode("B");
-        graph.AddNode("C");
-        graph.AddNode("D");
+        [Fact]
+        public void AddNode_ShouldAddNode()
+        {
+            // Arrange
+            Graph graph = new();
 
-        // Add edges
-        graph.AddEdge("A", "B");
-        graph.AddEdge("A", "C");
-        graph.AddEdge("B", "D");
-        graph.AddEdge("C", "D");
+            // Act
+            graph.AddNode("A");
 
-        // Perform BFS and DFS
-        List<string> bfsResult = graph.Bfs("A");
-        bfsResult.Should().Contain("A");
-    }
-    [Fact]
-    public void DFS_ShouldFindElement()
-    {
-        Graph graph = new();
+            // Assert
+            graph.Bfs("A").Should().Contain("A");
+        }
 
-        // Add nodes
-        graph.AddNode("A");
-        graph.AddNode("B");
-        graph.AddNode("C");
-        graph.AddNode("D");
+        [Fact]
+        public void AddEdge_ShouldAddEdge()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
 
-        // Add edges
-        graph.AddEdge("A", "B");
-        graph.AddEdge("A", "C");
-        graph.AddEdge("B", "D");
-        graph.AddEdge("C", "D");
+            // Act
+            graph.AddEdge("A", "B");
 
-        // Perform BFS and DFS
-        List<string> dfsResult = graph.Dfs("A");
-        dfsResult.Should().Contain("A");
+            // Assert
+            graph.Bfs("A").Should().Contain("B");
+        }
+
+        [Fact]
+        public void AddEdge_ShouldNotAddEdgeIfNodesDoNotExist()
+        {
+            // Arrange
+            Graph graph = new();
+
+            // Act
+            graph.AddEdge("A", "B");
+
+            // Assert
+            graph.Bfs("A").Should().BeEmpty();
+        }
+
+        [Fact]
+        public void BFS_ShouldFindElement()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddNode("D");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("A", "C");
+            graph.AddEdge("B", "D");
+            graph.AddEdge("C", "D");
+
+            // Act
+            List<string> bfsResult = graph.Bfs("A");
+
+            // Assert
+            bfsResult.Should().ContainInOrder("A", "B", "C", "D");
+        }
+
+        [Fact]
+        public void DFS_ShouldFindElement()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddNode("D");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("A", "C");
+            graph.AddEdge("B", "D");
+            graph.AddEdge("C", "D");
+
+            // Act
+            List<string> dfsResult = graph.Dfs("A");
+
+            // Assert
+            dfsResult.Should().ContainInOrder("A", "B", "D", "C");
+        }
+
+        [Fact]
+        public void BFS_ShouldHandleDisconnectedGraph()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddNode("D");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("C", "D");
+
+            // Act
+            List<string> bfsResult = graph.Bfs("A");
+
+            // Assert
+            bfsResult.Should().ContainInOrder("A", "B");
+            bfsResult.Should().NotContain("C");
+            bfsResult.Should().NotContain("D");
+        }
+
+        [Fact]
+        public void DFS_ShouldHandleDisconnectedGraph()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddNode("D");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("C", "D");
+
+            // Act
+            List<string> dfsResult = graph.Dfs("A");
+
+            // Assert
+            dfsResult.Should().ContainInOrder("A", "B");
+            dfsResult.Should().NotContain("C");
+            dfsResult.Should().NotContain("D");
+        }
+
+        [Fact]
+        public void BFS_ShouldHandleCycles()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("C", "A");
+
+            // Act
+            List<string> bfsResult = graph.Bfs("A");
+
+            // Assert
+            bfsResult.Should().ContainInOrder("A", "B", "C");
+        }
+
+        [Fact]
+        public void DFS_ShouldHandleCycles()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("C", "A");
+
+            // Act
+            List<string> dfsResult = graph.Dfs("A");
+
+            // Assert
+            dfsResult.Should().ContainInOrder("A", "B", "C");
+        }
+
+        [Fact]
+        public void BFS_ShouldReturnEmptyListForNonExistentStartNode()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+
+            // Act
+            List<string> bfsResult = graph.Bfs("C");
+
+            // Assert
+            bfsResult.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void DFS_ShouldReturnEmptyListForNonExistentStartNode()
+        {
+            // Arrange
+            Graph graph = new();
+            graph.AddNode("A");
+            graph.AddNode("B");
+
+            // Act
+            List<string> dfsResult = graph.Dfs("C");
+
+            // Assert
+            dfsResult.Should().BeEmpty();
+        }
     }
 }
