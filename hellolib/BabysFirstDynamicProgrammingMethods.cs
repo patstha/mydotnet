bbc580c08e62c3ad8efe95b95d7a1a4419b88f6d
Kushal <kushaldeveloper@gmail.com>
2024-11-01 07:07:20 -0400

    add some more stuff
    this is not dynamic programming, is it?

--- hellolib/BabysFirstDynamicProgrammingMethods.cs
+++ hellolib/BabysFirstDynamicProgrammingMethods.cs
@@ -2,5 +2,65 @@
 
 public class BabysFirstDynamicProgrammingMethods
 {
+    public static int FibonacciWithRecursion(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
     
+    static int FibonacciWithoutRecursion(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
+
+// using System;
+//
+// class Program
+// {
+//     static void Main()
+//     {
+//         int n = 10; // Example input
+//         Console.WriteLine($"Fibonacci({n}) = {Fibonacci(n)}");
+//     }
+//
+//     static int Fibonacci(int n)
+//     {
+//         if (n <= 1)
+//             return n;
+//
+//         int[] dp = new int[n + 1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//
+//         for (int i = 2; i <= n; i++)
+//         {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//
+//         return dp[n];
+//     }
+// }
