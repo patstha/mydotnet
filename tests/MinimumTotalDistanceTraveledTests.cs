b233aef6b00b79c93e0ea7d15f2b4e36b2853439
Kushal <kushaldeveloper@gmail.com>
2024-10-31 20:12:07 -0400

    add another failing test
    this is necessary so I don't just don't write
    return 4 and pass the test like that

--- tests/MinimumTotalDistanceTraveledTests.cs
+++ tests/MinimumTotalDistanceTraveledTests.cs
@@ -3,12 +3,28 @@
 public class MinimumTotalDistanceTraveledTests
 {
     [Fact]
-    public void MinimumTotalDistanceTraveled()
+    public void MinimumTotalDistanceTraveled0()
     {
         // arrange 
         List<int> robot = [0, 4, 6];
         int[][] factory = [[2, 2], [6, 2]];
-        long expected = 4;
+        const long expected = 4;
+        
+        // act 
+        MinimumTotalDistanceTraveled solution = new();
+        long actual = solution.MinimumTotalDistance(robot, factory);
+        
+        // assert 
+        actual.Should().Be(expected);
+    }
+    
+    [Fact]
+    public void MinimumTotalDistanceTraveled1()
+    {
+        // arrange 
+        List<int> robot = [1, -1];
+        int[][] factory = [[-2,1],[2,1]];
+        const long expected = 2;
         
         // act 
         MinimumTotalDistanceTraveled solution = new();
