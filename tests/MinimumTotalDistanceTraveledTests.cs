14909f195ac4a4f5d7fa90935a6a4c678ee30580
Kushal <kushaldeveloper@gmail.com>
2024-10-31 21:05:46 -0400

    add test
    Wrong Answer
    Runtime: 5 ms
    Case 1
    Case 2
    Case 3
    Input
    robot =
    [9,11,99,101]
    factory =
    [[10,1],[7,1],[14,1],[100,1],[96,1],[103,1]]
    Output
    93
    Expected
    6

--- tests/MinimumTotalDistanceTraveledTests.cs
+++ tests/MinimumTotalDistanceTraveledTests.cs
@@ -31,14 +31,14 @@ public class MinimumTotalDistanceTraveledTests
         // assert 
         actual.Should().Be(expected);
     }
-
+    
     [Fact]
     public void MinimumTotalDistanceTraveled2()
     {
         // arrange 
-        List<int> robot = [3, 8, 12];
-        int[][] factory = [[5, 1], [10, 2]];
-        const long expected = 7;
+        List<int> robot = [9,11,99,101];
+        int[][] factory = [[10,1],[7,1],[14,1],[100,1],[96,1],[103,1]];
+        const long expected = 6;
         
         // act 
         long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
@@ -47,138 +47,4 @@ public class MinimumTotalDistanceTraveledTests
         actual.Should().Be(expected);
     }
 
-    [Fact]
-    public void MinimumTotalDistanceTraveled3()
-    {
-        // arrange 
-        List<int> robot = [-5, -3, 2, 7];
-        int[][] factory = [[-4, 2], [6, 2]];
-        const long expected = 10;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled4()
-    {
-        // arrange 
-        List<int> robot = [0, 0, 0];
-        int[][] factory = [[0, 3]];
-        const long expected = 0;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled5()
-    {
-        // arrange 
-        List<int> robot = [1, 2, 3, 4];
-        int[][] factory = [[2, 2], [3, 2]];
-        const long expected = 2;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled6()
-    {
-        // arrange 
-        List<int> robot = [-10, -5, 0, 5, 10];
-        int[][] factory = [[-7, 1], [7, 1], [0, 3]];
-        const long expected = 25;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled7()
-    {
-        // arrange 
-        List<int> robot = [10, 20, 30];
-        int[][] factory = [[15, 1], [25, 2]];
-        const long expected = 20;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled8()
-    {
-        // arrange 
-        List<int> robot = [-1, -2, -3, -4];
-        int[][] factory = [[-2, 2], [-3, 2]];
-        const long expected = 2;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled9()
-    {
-        // arrange 
-        List<int> robot = [5, 10, 15, 20];
-        int[][] factory = [[10, 2], [20, 2]];
-        const long expected = 10;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled10()
-    {
-        // arrange 
-        List<int> robot = [0, 100, 200];
-        int[][] factory = [[50, 1], [150, 2]];
-        const long expected = 200;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void MinimumTotalDistanceTraveled11()
-    {
-        // arrange 
-        List<int> robot = [-100, -50, 0, 50, 100];
-        int[][] factory = [[-75, 1], [75, 1], [0, 3]];
-        const long expected = 250;
-        
-        // act 
-        long actual = MinimumTotalDistanceTraveled.MinimumTotalDistance(robot, factory);
-        
-        // assert 
-        actual.Should().Be(expected);
-    }
 }
