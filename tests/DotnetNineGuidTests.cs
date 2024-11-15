7a52a79afd20ed3c33597e49035a581c9f3431f1
Kushal <kushaldeveloper@gmail.com>
2024-11-14 20:20:57 -0500

    wip

--- tests/DotnetNineGuidTests.cs
+++ tests/DotnetNineGuidTests.cs
@@ -5,12 +5,12 @@ public class DotnetNineGuidTests
     [Fact]
     public async Task GenerateGuidv7_ShouldGenerateGuidv7_sequentially1()
     {
-        DotnetNineGuid firstObject = new();
+        DateTimeOffset startTime = DateTimeOffset.UtcNow;
+        DotnetNineGuid firstObject = new(TimestampProvider(10, startTime));
         Guid first = firstObject.GetGuid();
         long firstLong = DotnetNineGuid.ExtractTimestamp(first);
         DateTimeOffset firstDateTimeOffset = firstObject.GetTimestamp();
-        await Task.Delay(1);
-        DotnetNineGuid secondObject = new();
+        DotnetNineGuid secondObject = new(TimestampProvider(20, startTime));
         Guid second = secondObject.GetGuid();
         long secondLong = DotnetNineGuid.ExtractTimestamp(second);
         DateTimeOffset secondDateTimeOffset = secondObject.GetTimestamp();
@@ -18,19 +18,23 @@ public class DotnetNineGuidTests
         bool actual = secondLong > firstLong;
         firstDateTimeOffset.Should().BeBefore(secondDateTimeOffset);
         actual.Should().BeTrue();
+        return;
+        Func<DateTimeOffset> TimestampProvider(int delay, DateTimeOffset startTime) => startTime.AddMilliseconds(delay);
     }
 
     [Fact]
     public void GenerateGuidv7_ShouldGenerateGuidv7_sequentially2()
     {
-        List<DotnetNineGuid> objects = new List<DotnetNineGuid>();
+        List<DotnetNineGuid> objects = [];
         DateTimeOffset startTime = DateTimeOffset.UtcNow;
 
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < 1_000; i++)
         {
-            Func<DateTimeOffset> timestampProvider = () => startTime.AddMilliseconds(i);
-            DotnetNineGuid myObject = new DotnetNineGuid(timestampProvider);
+            int i1 = i;
+            DotnetNineGuid myObject = new(TimestampProvider);
             objects.Add(myObject);
+            continue;
+            DateTimeOffset TimestampProvider() => startTime.AddMilliseconds(i1);
         }
 
         for (int i = 0; i < objects.Count; i++)
