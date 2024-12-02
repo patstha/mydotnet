ca44d6699932f4fe610a6b4fef06cf0b2d85916c
kushal <kushaldeveloper@gmail.com>
2024-12-02 14:16:09 -0500

    prefer const

--- tests/MySearchTests.cs
+++ tests/MySearchTests.cs
@@ -6,98 +6,98 @@ public class MySearchTests
     public void LinearSearchTestDoesNotExist()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 9;
+        const int queryItem = 9;
         Assert.False(MySearch.LinearSearchIntegers(arrayToSearch, queryItem));
     }
     [Fact]
     public void LinearSearchTestDoesExist()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 4;
+        const int queryItem = 4;
         Assert.True(MySearch.LinearSearchIntegers(arrayToSearch, queryItem));
     }
     [Fact]
     public void BinarySearchDoesNotExist()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 9;
+        const int queryItem = 9;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(-1));
     }
     [Fact]
     public void BinarySearchDoesNotExistOdd()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6, 7];
-        int queryItem = 9;
+        const int queryItem = 9;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(-1));
     }
     [Fact]
     public void BinarySearchDoesExist()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 1;
+        const int queryItem = 1;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(0));
     }
     [Fact]
     public void BinarySearchDoesExistOdd()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6, 7];
-        int queryItem = 1;
+        const int queryItem = 1;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(0));
     }
     [Fact]
     public void BinarySearchDoesExist2()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 2;
+        const int queryItem = 2;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(1));
     }
     [Fact]
     public void BinarySearchDoesExistOdd2()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6, 7];
-        int queryItem = 2;
+        const int queryItem = 2;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(1));
     }
     [Fact]
     public void BinarySearchDoesExist3()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 3;
+        const int queryItem = 3;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(2));
     }
     [Fact]
     public void BinarySearchDoesExistOdd3()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6, 7];
-        int queryItem = 3;
+        const int queryItem = 3;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(2));
     }
     [Fact]
     public void BinarySearchDoesExist4()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6];
-        int queryItem = 4;
+        const int queryItem = 4;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(3));
     }
     [Fact]
     public void BinarySearchDoesExistOdd4()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 5, 6, 7];
-        int queryItem = 4;
+        const int queryItem = 4;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(3));
     }
     [Fact]
     public void BinarySearchDoesExist4Twice()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 4, 5, 6];
-        int queryItem = 4;
+        const int queryItem = 4;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(3));
     }
     [Fact]
     public void BinarySearchDoesExistOdd4Twice()
     {
         int[] arrayToSearch = [1, 2, 3, 4, 4, 5, 6, 7];
-        int queryItem = 4;
+        const int queryItem = 4;
         Assert.True(MySearch.BinarySearchIntegers(arrayToSearch, queryItem).Equals(3));
     }
     [Fact]
